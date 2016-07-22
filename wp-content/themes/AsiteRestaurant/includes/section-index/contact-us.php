9805bc027484da2ceb600b09a2716fe68f05cb7d
nguyenduy0502 <nguyenduy050294@gmail.com>
2016-07-22 23:35:15 +0300

    add contact form

--- wp-content/themes/AsiteRestaurant/includes/section-index/contact-us.php
+++ wp-content/themes/AsiteRestaurant/includes/section-index/contact-us.php
@@ -3,27 +3,19 @@
 		<div class="col-md-5 col-sm-5 col-xs-12">
 			<div class="contact-us-form">
 				<h2>Contact us</h2>
-				<div class="contact-info">
+			 <!--	<div class="contact-info">
 					<h6>Lorem ipsum dolor.</h6>
 					<h6>Lorem ipsum</h6>
 					<h6>Lorem</h6>
-				</div>
+				</div> -->
 				<div class="form-group">
-					<label for="name">Name:</label>
-					<input type="text" class="form-control">
-					<label for="email">Email:</label>
-					<input type="email" class="form-control">
-					<label for="comment">Comment:</label>
-					<textarea class="form-control" rows="5"></textarea>
-					<p>
-						<button type="button" class="btn btn-default btn-sm">Отправить</button>
-					</p>
+					<?php echo do_shortcode('[contact-form-7 id="3893" title="Contact form"]');?>
 				</div>
 			</div>
 		</div>
 		<div class="col-md-7 col-sm-7 col-xs-12">
 			<div class="google-map">
-				<img src="img/google-map.jpg" alt="">
+				<img src="<?php echo TEMPLATE_FOLDER.'/img/google-map.jpg'?>" alt="">
 			</div>
 		</div>
 	</div>
