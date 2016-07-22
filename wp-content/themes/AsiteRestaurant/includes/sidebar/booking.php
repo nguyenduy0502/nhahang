14b510d39c7929f2af05a565b294fdf898f4c1b9
nguyenduy0502 <nguyenduy050294@gmail.com>
2016-07-22 17:52:50 +0300

    base file booking

---
+++ wp-content/themes/AsiteRestaurant/includes/sidebar/booking.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * File Name: booking.php
+ * Date: 22-07-2016
+ * Time: 17:42
+ * Description: File active booking widget
+ */
+if (is_active_sidebar('booking-sidebar')):
+    dynamic_sidebar('booking-sidebar');
+endif;
+?>
