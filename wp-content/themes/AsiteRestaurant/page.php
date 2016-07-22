fcefa6bc6b65ad42197fb3190ad220f968305af2
nguyenduy0502 <nguyenduy050294@gmail.com>
2016-07-22 17:54:14 +0300

    base file page

---
+++ wp-content/themes/AsiteRestaurant/page.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * File Name: page.php
+ * Date: 22-07-2016
+ * Time: 15:42
+ * Description: Sample page
+ */
+?>
+<?php if(have_posts()):while(have_posts()):the_post();
+
+the_title();
+the_content();
+
+endwhile; endif; ?>
