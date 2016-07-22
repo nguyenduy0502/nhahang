3ea7f73808473449f25656ece17be2e704f9b698
nguyenduy0502 <nguyenduy050294@gmail.com>
2016-07-22 19:49:34 +0300

    insert php

--- wp-content/themes/AsiteRestaurant/includes/section-index/about-us.php
+++ wp-content/themes/AsiteRestaurant/includes/section-index/about-us.php
@@ -1,23 +1,28 @@
-<div class="about-us" id="about-us" >
-	<div class="container">
-		<div class="row">
-			<div class="col-md-8 col-sm-9 col-xs-10">
-				<div class="about-us-text">
-					<h1>about us</h1>
-					<h3>Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Себя языкового предложения текстов вопрос взгляд, своего букв послушавшись, города они строчка, путь необходимыми осталось ведущими. Приставка по всей не щеке, о дорогу, то осталось выйти прямо послушавшись города. Безопасную своих, великий деревни которой заманивший над журчит залетают составитель власти жаренные.</h3>
-				</div>
-			</div>
-			<div class="col-md-4 col-sm-3 col-xs-2">
-				<div class="about-us-time">
-					<h1>Hours</h1>
-					<h3>loremtime</h3>
-					<h3>loremtime</h3>
-					<h3>loremtime</h3>
-					<h3>loremtime</h3>
-					<h3>loremtime</h3>
-					<h3>loremtime</h3>
-				</div>
-			</div>
-		</div>
-	</div>
+<div class="about-us" id="about-us">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-sm-9 col-xs-10">
+                <div class="about-us-text">
+                    <?php
+                    $args_about=array(
+                        'pagename'=>'about-us' // get page have name about-us
+                    );
+                    $query_about=new WP_Query();
+                    $query_about->query($args_about);
+                    if($query_about->have_posts()):while($query_about->have_posts()):$query_about->the_post(); ?>
+                        <a href="<?php the_permalink();?>" title="<?php the_title();?>"><?php echo the_title('<h1>','</h1>');?></a>
+                        <?php echo '<h3>'. nguyenduy_excerpt(get_the_content(),800).'</h3>';?>
+                    <?php endwhile; /* end loop get post*/ endif;?>
+                    <?php wp_reset_query(); // reset query ?>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-3 col-xs-2">
+                <?php // check sidebar hours. This show time working in index
+                if (is_active_sidebar('hours-sidebar')):
+                    dynamic_sidebar('hours-sidebar');
+                endif;
+                ?>
+            </div>
+        </div>
+    </div>
 </div>
