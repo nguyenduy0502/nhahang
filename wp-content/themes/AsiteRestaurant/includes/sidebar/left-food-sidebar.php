8eac75595406ed2491303ecc23d4d2c4ecfd7adf
nguyenduy0502 <nguyenduy050294@gmail.com>
2016-07-28 02:26:37 +0300

    update 28/07 2:26

---
+++ wp-content/themes/AsiteRestaurant/includes/sidebar/left-food-sidebar.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * File Name: left-food-sidebar.php
+ * Date: 28-07-2016
+ * Time: 0:22
+ * Description:
+ */
+?>
+<?php if(is_active_sidebar('left-food-sidebar')): ?>
+    <?php dynamic_sidebar('left-food-sidebar');?>
+<?php endif;?>
