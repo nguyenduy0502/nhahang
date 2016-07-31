2adb976d7070a7e18ac4f85e6765865e65cff3b4
nguyenduy0502 <nguyenduy050294@gmail.com>
2016-08-01 01:20:22 +0300

    add meta keywords

--- wp-content/themes/AsiteRestaurant/header.php
+++ wp-content/themes/AsiteRestaurant/header.php
@@ -1,85 +1,89 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]><html lang="ru" class="lt-ie9 lt-ie8 lt-ie7"><![endif]-->
-<!--[if IE 7]><html lang="ru" class="lt-ie9 lt-ie8"><![endif]-->
-<!--[if IE 8]><html lang="ru" class="lt-ie9"><![endif]-->
+<!--[if lt IE 7]>
+<html lang="ru" class="lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]>
+<html lang="ru" class="lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]>
+<html lang="ru" class="lt-ie9"><![endif]-->
 <!--[if gt IE 8]><!-->
 <html lang="ru">
 <!--<![endif]-->
 <head>
-	<meta charset="<?php bloginfo('charset');?>" />
-	<title>
-		<?php
-		$cat_name = get_the_category();
-		if (is_home()) {
-			// Title home
-			bloginfo('name');
-			echo "|";
-			bloginfo('description');
-		}
-		if (is_single()) {
-			// Title single
-			the_title();
-			echo "|";
-			bloginfo('name');
-		}
-		if (is_category()) {
-			// title category
-			echo $cat_name[0]->cat_name;
-			echo "|";
-			bloginfo('name');
-		}
-		if (is_404()) {
-			// title 404
-			echo 'Ошибка 404. Не найдено.' . '|';
-			bloginfo('name');
-		}
-		if (is_page()) {
-			// title page
-			the_title();
-			echo "|";
-			bloginfo('description');
-		}
-		if (is_search()) {
-			// title search
-			echo 'Результаты поиска' . '|';
-			bloginfo('name');
-		}
-		if (is_author()) {
-			// title author
-			echo 'Наш шеф-повар' . '|';
-			bloginfo('name');
-		}
-		if (is_post_type_archive('food')) {
-			// title archive company
-			echo "Новые блюда в нашем ресторане" . "|";
-			bloginfo('name');
-		}
-		if (is_post_type_archive('menu')) {
-			// title archive event
-			echo "Меню нашего ресторана"."|";
-			bloginfo('name');
-		}
-		if (is_tax('cuisine')) {
-			$term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
-			echo $term->name;
-			echo "|";
-			bloginfo('name');
-		}
+    <meta charset="<?php bloginfo('charset'); ?>"/>
+    <title>
+        <?php
+        $cat_name = get_the_category();
+        if (is_home()) {
+            // Title home
+            bloginfo('name');
+            echo "|";
+            bloginfo('description');
+        }
+        if (is_single()) {
+            // Title single
+            the_title();
+            echo "|";
+            bloginfo('name');
+        }
+        if (is_category()) {
+            // title category
+            echo $cat_name[0]->cat_name;
+            echo "|";
+            bloginfo('name');
+        }
+        if (is_404()) {
+            // title 404
+            echo 'Ошибка 404. Не найдено.' . '|';
+            bloginfo('name');
+        }
+        if (is_page()) {
+            // title page
+            the_title();
+            echo "|";
+            bloginfo('description');
+        }
+        if (is_search()) {
+            // title search
+            echo 'Результаты поиска' . '|';
+            bloginfo('name');
+        }
+        if (is_author()) {
+            // title author
+            echo 'Наш шеф-повар' . '|';
+            bloginfo('name');
+        }
+        if (is_post_type_archive('food')) {
+            // title archive company
+            echo "Новые блюда в нашем ресторане" . "|";
+            bloginfo('name');
+        }
+        if (is_post_type_archive('menu')) {
+            // title archive event
+            echo "Меню нашего ресторана" . "|";
+            bloginfo('name');
+        }
+        if (is_tax('cuisine')) {
+            $term = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy'));
+            echo $term->name;
+            echo "|";
+            bloginfo('name');
+        }
 
-		?>
-	</title>
-	<meta content="telephone=no" name="format-detection" />
-	<meta http-equiv="x-rim-auto-match" content="none">
-
-	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-	<link rel="shortcut icon" href="<?php echo TEMPLATE_FOLDER.'/img/logo.jpg'?>" />
-	<meta property="fb:app_id" content="273681262992709" />
-	<meta property="fb:admins" content="1099208330101074"/>
+        ?>
+    </title>
+    <meta name="author" content="<?php bloginfo('name');?>">
+    <meta content="telephone=no" name="format-detection"/>
+    <meta http-equiv="x-rim-auto-match" content="none">
+    <meta name="Keywords"
+          content="вьетнамская кухня, вьетнамская кухня рецепты, ресторан вьетнамской кухни, блюда вьетнамской кухни, кафе вьетнамской кухни, вьетнамская кухня в москве, суп фо бо, вьетнамский суп фо, фо рецепт, Вьет Хауз, суп фо вьетнамский рецепт, вьетнамская кухня меню, ВьетЛанч, <?php wp_title( '|', true, 'right' );?>"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="shortcut icon" href="<?php echo TEMPLATE_FOLDER . '/img/logo.jpg' ?>"/>
+    <meta property="fb:app_id" content="273681262992709"/>
+    <meta property="fb:admins" content="1099208330101074"/>
 
 </head>
-<?php wp_head();?>
+<?php wp_head(); ?>
 <body>
 <!-- header-->
 <!--
