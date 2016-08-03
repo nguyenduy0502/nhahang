0c4a9293479c46834c9e61678c4e84528d1255f3
Dinh Trung Hieu <foreverisme2002@gmail.com>
2016-08-04 01:11:33 +0300

    single style update

--- wp-content/themes/AsiteRestaurant/includes/relatedpost/cat_post.php
+++ wp-content/themes/AsiteRestaurant/includes/relatedpost/cat_post.php
@@ -6,24 +6,24 @@
  * Description:
  */
 ?>
-<div class="spc-bottom clearfix">
+<div class="page-bottom clearfix">
     <div class="col-md-12">
         <h2>Возможно, Вас заинтересует:</h2>
     </div>
     <div class="col-md-4">
-        <div class="spc-otherpost">
+        <div class="pb-otherpost">
             <img src="http://lorempixel.com/250/150/food" alt="">
             <p>Далеко-далеко за словесными горами в стране.</p>
         </div>
     </div>
     <div class="col-md-4">
-        <div class="spc-otherpost">
+        <div class="pb-otherpost">
             <img src="http://lorempixel.com/250/150/food" alt="">
             <p>Далеко-далеко за словесными горами в стране.</p>
         </div>
     </div>
     <div class="col-md-4">
-        <div class="spc-otherpost">
+        <div class="pb-otherpost">
             <img src="http://lorempixel.com/250/150/food" alt="">
             <p>Далеко-далеко за словесными горами в стране.</p>
         </div>
