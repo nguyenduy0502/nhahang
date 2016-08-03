20d4d530a60dc9a407e400f9c5b6fadb2696d4e5
nguyenduy0502 <nguyenduy050294@gmail.com>
2016-08-04 01:06:55 +0300

    single update

---
+++ wp-content/themes/AsiteRestaurant/includes/post-format/gallery.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * File Name: gallery.php
+ * Date: 04-08-2016
+ * Time: 0:52
+ * Description:Content post type gallery
+ */
+?>
---
+++ wp-content/themes/AsiteRestaurant/includes/post-format/standard.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * File Name: standard.php
+ * Date: 04-08-2016
+ * Time: 0:51
+ * Description: Content post type standard
+ */
+?>
+<?php $cat=get_the_category(); ?>
+<div class="single-page-name">
+    <h2><?php the_title();?></h2>
+</div>
+<div class="spc-header">
+    <img src="<?php the_post_thumbnail_url('large');?>" alt="<?php the_title();?>" title="<?php the_title();?>">
+    <div class="post-info">
+        <a href="<?php the_permalink();?>" rel="bookmark"></a>
+        <time class="entry-date published"
+              datetime="<?php echo get_the_date('Y-m-d H:i:s'); ?>"><?php echo get_the_date('d-M-Y'); ?></time>
+        <time class="updated"
+              datetime="<?php the_modified_time('d-M-Y H:i:s'); ?>"><?php the_modified_time('d-M-Y'); ?></time>
+        <a href="<?php echo get_category_link($cat[0]->term_id);?>" rel="category tag"><span><?php echo $cat[0]->name;?></span></a>
+    </div><!-- -->
+</div>
+<div class="spc-content"><?php the_content();?></div>
