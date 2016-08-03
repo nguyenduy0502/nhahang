881bd11fdb47ac9dac80560704131ca03d930007
nguyenduy0502 <nguyenduy050294@gmail.com>
2016-08-04 00:00:20 +0300

    base file related type post

---
+++ wp-content/themes/AsiteRestaurant/includes/relatedpost/cat_post.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * File Name: cat_post.php
+ * Date: 03-08-2016
+ * Time: 23:50
+ * Description:
+ */
+?>
+<div class="spc-bottom clearfix">
+    <div class="col-md-12">
+        <h2>Возможно, Вас заинтересует:</h2>
+    </div>
+    <div class="col-md-4">
+        <div class="spc-otherpost">
+            <img src="http://lorempixel.com/250/150/food" alt="">
+            <p>Далеко-далеко за словесными горами в стране.</p>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="spc-otherpost">
+            <img src="http://lorempixel.com/250/150/food" alt="">
+            <p>Далеко-далеко за словесными горами в стране.</p>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="spc-otherpost">
+            <img src="http://lorempixel.com/250/150/food" alt="">
+            <p>Далеко-далеко за словесными горами в стране.</p>
+        </div>
+    </div>
+</div>
