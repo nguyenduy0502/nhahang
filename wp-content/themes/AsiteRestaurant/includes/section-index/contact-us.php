0b349d75c4a132775c9276eea7401d161bc9bb23
nguyenduy0502 <nguyenduy050294@gmail.com>
2016-08-29 00:10:25 +0300

    add code contact

--- wp-content/themes/AsiteRestaurant/includes/section-index/contact-us.php
+++ wp-content/themes/AsiteRestaurant/includes/section-index/contact-us.php
@@ -1,132 +1,127 @@
 <!-- <div class="contact-us section" id="section5" data-anchor="contact-us"> -->
+<?php global $asite_options; // global variable theme option ?>
 <div class="contact-us section" id="contact-us">
     <div class="gallery col-md-6">
-       <div class="section-title">
-           <h1>Что</h1>
-           <h2>О нас думают</h2>
-       </div>
-    <div class="contact-us-background"></div>
-  <div class="col-md-6">
-    <div class="ih-item square colored effect6 bottom_to_top"><a href="#">
-        <div class="img"><img src="http://viethousecafe.ru/wp-content/uploads/2016/08/1-slider.jpg" alt="img" alt="img"></div>
-        <div class="info">
-          <h3>Heading here</h3>
-          <p>Description goes here</p>
-        </div></a></div>
-  </div>
-  <div class="col-md-6">
-    <div class="ih-item square colored effect6 bottom_to_top"><a href="#">
-        <div class="img"><img src="http://viethousecafe.ru/wp-content/uploads/2016/08/1-slider.jpg" alt="img" alt="img"></div>
-        <div class="info">
-          <h3>Heading here</h3>
-          <p>Description goes here</p>
-        </div></a></div>
-  </div>
-  <div class="col-md-6">
-    <div class="ih-item square colored effect6 bottom_to_top"><a href="#">
-        <div class="img"><img src="http://viethousecafe.ru/wp-content/uploads/2016/08/1-slider.jpg" alt="img" alt="img"></div>
-        <div class="info">
-          <h3>Heading here</h3>
-          <p>Description goes here</p>
-        </div></a></div>
-  </div>
-  <div class="col-md-6">
-    <div class="ih-item square colored effect6 bottom_to_top"><a href="#">
-        <div class="img"><img src="http://viethousecafe.ru/wp-content/uploads/2016/08/1-slider.jpg" alt="img" alt="img"></div>
-        <div class="info">
-          <h3>Heading here</h3>
-          <p>Description goes here</p>
-        </div></a></div>
-  </div>
-    <div class="col-md-12">
-        <div class="owl-carousel2 testimonial">
-            <div class="item-testm">
-                <div class="item-testm-content">
-                    <div class="item-testm-pers">
-                        <div class="item-testm-pers-img">
-                            <img src="http://static2.businessinsider.com/image/564e42f82491f92b008b6af2-480/david-beckham.jpg" alt="">
+        <div class="section-title">
+            <h1>Что</h1>
+            <h2>О нас думают</h2>
+        </div>
+        <div class="contact-us-background"></div>
+        <div class="col-md-12">
+            <div class="owl-carousel2 testimonial">
+                <?php
+                $args=array(
+                    'orderby'=>'comment_date',
+                    'status'=>'approve',
+                    'number'=>10
+                );
+                $comments=get_comments($args);
+                ?>
+                <?php foreach($comments as $comment):?>
+                <div class="item-testm">
+                    <div class="item-testm-content">
+                        <div class="item-testm-pers">
+                            <div class="item-testm-pers-img">
+                               <?php get_avatar($comment)?>
+                            </div>
+                        </div>
+                        <div class="item-testm-text">
+                            <blockquote><?php echo $comment->comment_content?><cite> - <?php echo $comment->comment_author;?></cite></blockquote>
                         </div>
                     </div>
-                    <div class="item-testm-text">
-                        <blockquote>
-                        Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Напоивший первую строчка назад проектах гор заглавных, ручеек несколько которой!<cite> - Владимир</cite>
-                        </blockquote>
-
-                    </div>
                 </div>
-            </div>
-            <div class="item-testm">
-                <div class="item-testm-content">
-                    <div class="item-testm-pers">
-                        <div class="item-testm-pers-img">
-                            <img src="http://static2.businessinsider.com/image/564e42f82491f92b008b6af2-480/david-beckham.jpg" alt="">
-                        </div>
-                    </div>
-                    <div class="item-testm-text">
-                        <blockquote>
-                        Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Напоивший первую строчка назад проектах гор заглавных, ручеек несколько которой!<cite> - Владимир</cite>
-                        </blockquote>
-
-                    </div>
-                </div>
-            </div>
-            <div class="item-testm">
-                <div class="item-testm-content">
-                    <div class="item-testm-pers">
-                        <div class="item-testm-pers-img">
-                            <img src="http://static2.businessinsider.com/image/564e42f82491f92b008b6af2-480/david-beckham.jpg" alt="">
-                        </div>
-                    </div>
-                    <div class="item-testm-text">
-                        <blockquote>
-                        Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Напоивший первую строчка назад проектах гор заглавных, ручеек несколько которой!<cite> - Владимир</cite>
-                        </blockquote>
-
-                    </div>
-                </div>
+                <?php endforeach; ?>
             </div>
         </div>
+        <div class="col-md-6">
+            <div class="ih-item square colored effect6 bottom_to_top"><a href="#">
+                    <div class="img"><img src="http://viethousecafe.ru/wp-content/uploads/2016/08/1-slider.jpg"
+                                          alt="img" alt="img"></div>
+                    <div class="info">
+                        <h3>Heading here</h3>
+
+                        <p>Description goes here</p>
+                    </div>
+                </a></div>
+        </div>
+        <div class="col-md-6">
+            <div class="ih-item square colored effect6 bottom_to_top"><a href="#">
+                    <div class="img"><img src="http://viethousecafe.ru/wp-content/uploads/2016/08/1-slider.jpg"
+                                          alt="img" alt="img"></div>
+                    <div class="info">
+                        <h3>Heading here</h3>
+
+                        <p>Description goes here</p>
+                    </div>
+                </a></div>
+        </div>
+        <div class="col-md-6">
+            <div class="ih-item square colored effect6 bottom_to_top"><a href="#">
+                    <div class="img"><img src="http://viethousecafe.ru/wp-content/uploads/2016/08/1-slider.jpg"
+                                          alt="img" alt="img"></div>
+                    <div class="info">
+                        <h3>Heading here</h3>
+
+                        <p>Description goes here</p>
+                    </div>
+                </a></div>
+        </div>
+        <div class="col-md-6">
+            <div class="ih-item square colored effect6 bottom_to_top"><a href="#">
+                    <div class="img"><img src="http://viethousecafe.ru/wp-content/uploads/2016/08/1-slider.jpg"
+                                          alt="img" alt="img"></div>
+                    <div class="info">
+                        <h3>Heading here</h3>
+
+                        <p>Description goes here</p>
+                    </div>
+                </a></div>
+        </div>
+
     </div>
-        </div>
-        <div class="contact-us-info col-md-6">
-            <div class="contact-us-content">
-                    <div class="section-title">
-                        <h1>Свяжитесь</h1>
-                        <h2>С нами</h2>
-                    </div>
+    <div class="contact-us-info col-md-6">
+        <div class="contact-us-content">
+            <div class="section-title">
+                <h1>Свяжитесь</h1>
+
+                <h2>С нами</h2>
+            </div>
             <div class="col-md-5 col-sm-5 col-xs-12">
                 <div class="contact-us-form">
                     <h2>Контакты</h2>
-                    <!--	<div class="contact-info">
-                           <h6>Lorem ipsum dolor.</h6>
-                           <h6>Lorem ipsum</h6>
-                           <h6>Lorem</h6>
-                       </div> -->
-                    <div class="form-group">
-                        <?php echo do_shortcode('[contact-form-7 id="3872" title="Contact form"]'); ?>
-                    </div>
+                    <?php if(!empty($asite_options['option_contact'])&& isset($asite_options['option_contact'])): // check option contact in theme option?>
+                        <div class="form-group">
+                            <?php echo do_shortcode($asite_options['option_contact']); ?>
+                        </div>
+                    <?php endif; // end check option_contact?>
                 </div>
             </div>
+            <?php if(!empty($asite_options['option_map']) && isset($asite_options['option_map'])): // check option on/off map in theme option ?>
             <div class="col-md-7 col-sm-7 col-xs-12">
                 <div class="google-map">
-                    <div class="map-header" >
-                        <div id="marker-content">
-                            <p>ул. Покровка, 14/2, стр 01, Moscow</p>
-                            <p>Телефон: 8 (926) 127-37-77</p>
-                        </div>
+                    <div class="map-header">
+                        <?php if(!empty($asite_options['option_marker']) && isset($asite_options['option_marker'])): ?>
+                        <div id="marker-content"><?php echo $asite_options['option_marker']?></div>
+                    <?php else: ?>
+                            <div id="marker-content">ул. Покровка, 14/2, стр 01</div>
+                    <?php endif; // end check option_marker (text in marker)?>
                     </div>
-                    <div id="map-canvas" class="location-map" ></div>
+                    <div id="map-canvas" class="location-map"></div>
                     <script type='text/javascript'>
                         /* <![CDATA[ */
-                        var wpdata = {"url":"http:\/\/viethousecafe.ru\/\/","td":"http:\/\/viethousecafe.ru\/wp-content\/themes\/AsiteRestaurant\/template\/viethouse\/img\/"};
-                        var geo = {"map":"1","lat":"55.7581792","lon":"37.6432273"};
-                        var flickr = {"id":"52617155@N08","limit":"15"};
-                        var color = {"code":"#fc6f5c"};
+                        var wpdata = {
+                            "url": "http:\/\/viethousecafe.ru\/\/",
+                            "td": "http:\/\/viethousecafe.ru\/wp-content\/themes\/AsiteRestaurant\/template\/viethouse\/img\/"
+                        };
+                        var geo = {"map": "1", "lat": "<?php echo $asite_options['option_lat']?>", "lon": "<?php echo $asite_options['option_lon']?>"};
+                        var flickr = {"id": "52617155@N08", "limit": "15"};
+                        var color = {"code": "#fc6f5c"};
                         /* ]]> */
                     </script>
                 </div>
             </div>
-            </div>
+            <?php endif; // end check option_map?>
         </div>
-        <a href="#" class="scrollup"> button</a>
+    </div>
+    <a href="#" class="scrollup"> button</a>
 </div>
