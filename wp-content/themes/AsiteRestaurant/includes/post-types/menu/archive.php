e6f52d6c7b706da6766714c8ded6401b430118ae
nguyenduy0502 <nguyenduy050294@gmail.com>
2016-09-18 17:06:15 +0300

    updated 18/9

--- wp-content/themes/AsiteRestaurant/includes/post-types/menu/archive.php
+++ wp-content/themes/AsiteRestaurant/includes/post-types/menu/archive.php
@@ -13,7 +13,8 @@
             <div class="archive-menu-name">
                 <h2>Список Меню</h2>
             </div>
-            <?php if (have_posts()):while (have_posts()):the_post(); ?>
+            <?php $query = new WP_Query(array('posts_per_page'=>'-1','post_type' => 'menu'));?>
+            <?php if ($query->have_posts()):while ($query->have_posts()):$query->the_post(); ?>
                 <?php
                 $time_start=get_post_meta($post->ID,'time_start',true);
                 $time_end=get_post_meta($post->ID,'time_end',true);
@@ -44,6 +45,7 @@
                      </div> -->
                 </div>
                 <?php get_template_part('includes/tags/google-tags'); // google tag SEO?>
+                <?php wp_reset_query();?>
             <?php endwhile; endif; ?>
         </div>
     </div>
