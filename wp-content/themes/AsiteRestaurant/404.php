9d9e2b7e367d4018287e0f71c01ff9427ecda840
nguyenduy0502 <nguyenduy050294@gmail.com>
2016-09-28 16:49:10 +0300

    add home_url

--- wp-content/themes/AsiteRestaurant/404.php
+++ wp-content/themes/AsiteRestaurant/404.php
@@ -4,9 +4,7 @@ get_header();?>
 	<div class="e404">
 		<h1>404</h1>
 		<h2>Ошибка.</h2>
-		<h3><a href=""><<вернуться</a></h3>
+		<h3><a href="<?php echo home_url();?>"><<Вернуться</a></h3>
 	</div>
 </div>
-
-
 <?php get_footer();?>
