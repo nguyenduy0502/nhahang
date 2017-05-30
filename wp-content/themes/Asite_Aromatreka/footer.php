fe9112db70ca120ab909b14a5d593ddb84c81b08
Hieu <chdin@student.mesi.ru>
2017-05-30 14:12:16 +0300

    front end 0.4

--- wp-content/themes/Asite_Aromatreka/footer.php
+++ wp-content/themes/Asite_Aromatreka/footer.php
@@ -6,63 +6,64 @@
     <div class="inner-middle">
         <div class="container">
             <div class="row">
-                <div class="col-md-4">
-                    <!-- Footer Address -->
-                    <div class="footer_adress">
-                        <div class="footer_adress_head">
-                            Адрес:
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <!-- Footer Address -->
+                            <div class="footer_adress">
+                                <div class="footer_adress_head">
+                                    Адрес:
+                                </div>
+                                <?php echo !empty($asite_options['option_contact_address']) ?
+                                    ' <div class="footer_adress_subhead">' . $asite_options['option_contact_address'] . '</div>' : '' ?>
+                            </div>
+                            <!-- END Footer Address -->
+                            <!-- Footer Social -->
+                            <div class="footer_social">
+                                <?php echo !empty($asite_options['option_socail_fb']) ?
+                                    '<a href=' . $asite_options['option_socail_fb'] . '><img src=' . IMG_FOLDER . '/pink-social/004-social.png' . '></a>' : '' ?>
+
+                                <?php echo !empty($asite_options['option_socail_insta']) ?
+                                    '<a href=' . $asite_options['option_socail_insta'] . '><img src=' . IMG_FOLDER . '/pink-social/003-instagram-logo.png' . '></a>' : '' ?>
+
+                                <?php echo !empty($asite_options['option_socail_mail']) ?
+                                    '<a href=' . $asite_options['option_socail_mail'] . '><img src=' . IMG_FOLDER . '/pink-social/002-email.png' . '></a>' : '' ?>
+
+                                <?php echo !empty($asite_options['option_socail_vk']) ?
+                                    '<a href=' . $asite_options['option_socail_vk'] . '><img src=' . IMG_FOLDER . '/pink-social/001-vk-social-logotype.png' . '></a>' : '' ?>
+                            </div>
+                            <!-- END Footer Social -->
                         </div>
-                        <?php echo !empty($asite_options['option_contact_address']) ?
-                            ' <div class="footer_adress_subhead">' . $asite_options['option_contact_address'] . '</div>' : '' ?>
+                        <div class="col-md-6">
+                            <!-- Footer Contact -->
+                            <div class="footer_contact">
+                                <div class="footer_contact_head">
+                                    Телефон:
+                                </div>
+                                <div class="footer_contact_subhead">
+                                    <ul>
+                                        <?php echo !empty($asite_options['option_contact_phone_1']) ?
+                                            '<li>' . $asite_options['option_contact_phone_1'] . '</li>' : '';
+                                        ?>
+                                        <?php echo !empty($asite_options['option_contact_phone_2']) ?
+                                            '<li>' . $asite_options['option_contact_phone_2'] . '</li>' : '';
+                                        ?>
+                                        <?php echo !empty($asite_options['option_contact_phone_3']) ?
+                                            '<li>' . $asite_options['option_contact_phone_3'] . '</li>' : '';
+                                        ?>
 
-                    </div>
-                    <!-- END Footer Address -->
-                </div>
-                <div class="col-md-4">
-                    <!-- Footer Contact -->
-                    <div class="footer_contact">
-                        <div class="footer_contact_head">
-                            Телефон:
+                                    </ul>
+                                </div>
+                            </div>
+                            <!-- END Footer Contact -->
                         </div>
-                        <div class="footer_contact_subhead">
-                            <ul>
-                                <?php echo !empty($asite_options['option_contact_phone_1']) ?
-                                    '<li>' . $asite_options['option_contact_phone_1'] . '</li>' : '';
-                                ?>
-                                <?php echo !empty($asite_options['option_contact_phone_2']) ?
-                                    '<li>' . $asite_options['option_contact_phone_2'] . '</li>' : '';
-                                ?>
-                                <?php echo !empty($asite_options['option_contact_phone_3']) ?
-                                    '<li>' . $asite_options['option_contact_phone_3'] . '</li>' : '';
-                                ?>
-
-                            </ul>
+                        <div class="col-md-12 no-padding">
+                            <div id='map'></div>
                         </div>
                     </div>
-                    <!-- END Footer Contact -->
                 </div>
-                <div class="col-md-4">
-                    <!-- Footer Social -->
-                    <div class="footer_social">
-                        <?php echo !empty($asite_options['option_socail_fb']) ?
-                            '<a href=' . $asite_options['option_socail_fb'] . '><img src=' . IMG_FOLDER . '/pink-social/004-social.png' . '></a>' : '' ?>
-
-                        <?php echo !empty($asite_options['option_socail_insta']) ?
-                            '<a href=' . $asite_options['option_socail_insta'] . '><img src=' . IMG_FOLDER . '/pink-social/003-instagram-logo.png' . '></a>' : '' ?>
-
-                        <?php echo !empty($asite_options['option_socail_mail']) ?
-                            '<a href=' . $asite_options['option_socail_mail'] . '><img src=' . IMG_FOLDER . '/pink-social/002-email.png' . '></a>' : '' ?>
-
-                        <?php echo !empty($asite_options['option_socail_vk']) ?
-                            '<a href=' . $asite_options['option_socail_vk'] . '><img src=' . IMG_FOLDER . '/pink-social/001-vk-social-logotype.png' . '></a>' : '' ?>
-
-                    </div>
-                    <!-- END Footer Social -->
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12 no-padding">
-                    <div id='map'></div>
+                <div class="col-md-6">
+                    <?php echo rtb_print_booking_form(); ?>
                 </div>
             </div>
         </div>
