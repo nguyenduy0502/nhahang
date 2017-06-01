4f295b74bf96e3a3189f54a93274f70e39dea633
nguyenduy0502 <nguyenduy050294@gmail.com>
2017-06-01 16:36:17 +0300

    FULL v5.0 update 404

--- wp-content/themes/Asite_Aromatreka/404.php
+++ wp-content/themes/Asite_Aromatreka/404.php
@@ -1,1 +1,12 @@
-loi 404
+<?php get_header();?>
+<!-- Section Menu -->
+<div class="error">
+    <div class="container">
+        <div class="text-center">
+            <h1>404 error.</h1>
+            <h2><a href="<?php echo home_url();?>">back to main</a></h2>
+        </div>
+    </div>
+</div>
+<!--END Section Menu -->
+<?php get_footer();?>
